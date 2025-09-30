2922cb1c387ba96dcca2647dedee378bb00b3659
Antônio Prado <antonioeliaspr@gmail.com>
2025-09-30 16:54:52 -0300

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module aantonioprado/rs-go-api-movie-search
+
+go 1.23.3
+
+require github.com/go-chi/chi/v5 v5.2.3
---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"aantonioprado/rs-go-api-movie-search/api"
+	"log/slog"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	if err := run(); err != nil {
+		slog.Error("Failed to execute code", "error", err)
+		os.Exit(1)
+	}
+
+	slog.Info("Code executed successfully")
+}
+
+func run() error {
+	handler := api.NewHandler()
+
+	s := http.Server{
+		Addr:         ":8080",
+		Handler:      handler,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  time.Minute,
+	}
+
+	if err := s.ListenAndServe(); err != nil {
+		return err
+	}
+
+	return nil
+}
